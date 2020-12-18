7d5e712f5a194fe2a38f69d9a1176c60b1fdb7cc
MattElmer <matthias.elmer@gmail.com>
2020-12-18 14:16:01 -0700

    \n is good enough

--- canvas.js
+++ canvas.js
@@ -1,16 +1,20 @@
 const tuple = require('./tuple')
-const os = require('os')
 const BLACK = tuple.color(0, 0, 0)
 const MAX_RGB = 255
+const HEADER = `\
+P3
+${this.width} ${this.height}
+${MAX_RGB}
+`
 exports.canvas = class {
     constructor(w, h) {
         this.arr = Array.from({length:h}, () => Array(w).fill(BLACK))
     }
     write_pixel   = (x, y, c) => this.arr[y][x] = c
     pixel_at      = (x, y)    => this.arr[y][x]
-    canvas_to_ppm = ()        => `P3${os.EOL}${this.width} ${this.height}${os.EOL}${MAX_RGB}${os.EOL}`
+    canvas_to_ppm = ()        => HEADER
 }
 Object.defineProperties(exports.canvas.prototype, {
-    'width':  { get: function() { return this.find(Boolean).length } },
+    'width':  { get: function() { return this.arr.find(Boolean).length } },
     'height': { get: function() { return this.arr.length } }
 })
