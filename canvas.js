7de41d131f48de0a3767d8fb92e2bdf039d6b885
MattElmer <matthias.elmer@gmail.com>
2020-12-18 23:38:48 -0700

    scale components

--- canvas.js
+++ canvas.js
@@ -1,12 +1,12 @@
-const tuple = require('./tuple')
-const BLACK = tuple.color(0, 0, 0)
+const { color, mul } = require('./tuple')
+const BLACK = Object.freeze(color(0, 0, 0))
 const MAX_RGB = 255
 const HEADER = `\
 P3
 ${this.width} ${this.height}
 ${MAX_RGB}
 `
-MAX_LEN = 70
+const MAX_LEN = 70
 exports.canvas = class {
     constructor(w, h) {
         this.arr = Array.from({length:h}, () => Array(w).fill(BLACK))
@@ -14,8 +14,8 @@ exports.canvas = class {
     write_pixel   = (x, y, c) => this.arr[y][x] = c
     pixel_at      = (x, y)    => this.arr[y][x]
     canvas_to_ppm = ()        => HEADER // + arr.flatmap(row =>
-                                        //       row.flatmap(col =>
-                                        //          col.arr))
+                                        //      row.flatmap(col =>
+                                        //          mul(col, MAX_RGB).arr))
                                         //      .join(' ')
                                         //      .split(???)
                                         //      .join('\n') + '\n'
