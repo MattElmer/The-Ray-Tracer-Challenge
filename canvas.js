6c5ed03fcfea9ddaa125bac154d8e9400a2263b7
MattElmer <matthias.elmer@gmail.com>
2020-12-18 18:49:51 -0700

    plan of attack

--- canvas.js
+++ canvas.js
@@ -6,13 +6,19 @@ P3
 ${this.width} ${this.height}
 ${MAX_RGB}
 `
+MAX_LEN = 70
 exports.canvas = class {
     constructor(w, h) {
         this.arr = Array.from({length:h}, () => Array(w).fill(BLACK))
     }
     write_pixel   = (x, y, c) => this.arr[y][x] = c
     pixel_at      = (x, y)    => this.arr[y][x]
-    canvas_to_ppm = ()        => HEADER
+    canvas_to_ppm = ()        => HEADER // + arr.flatmap(row =>
+                                        //       row.flatmap(col =>
+                                        //          col.arr))
+                                        //      .join(' ')
+                                        //      .split(???)
+                                        //      .join('\n') + '\n'
 }
 Object.defineProperties(exports.canvas.prototype, {
     'width':  { get: function() { return this.arr.find(Boolean).length } },
