c47865fa8304e0ea5c1378a3662486cdf2afda23
MattElmer <matthias.elmer@gmail.com>
2020-12-20 03:09:46 -0700

    I dunno

--- canvas.js
+++ canvas.js
@@ -1,4 +1,3 @@
-const { stripIndent } = require('common-tags')
 const { color } = require('./tuple')
 const BLACK = Object.freeze(color(0, 0, 0))
 const MAX_RGB = 255
@@ -9,19 +8,19 @@ exports.canvas = class {
     }
     write_pixel   = (x, y, c) => this.arr[y][x] = c
     pixel_at      = (x, y)    => this.arr[y][x]
-    canvas_to_ppm = ()        => stripIndent`
-        P3
-        ${this.width} ${this.height}
-        ${MAX_RGB}` + '\n' +
+    canvas_to_ppm = ()        => `\
+P3
+${this.width} ${this.height}
+${MAX_RGB}` + '\n' +
         arr.flatmap(row =>
             row.flatmap(col =>
                 col.arr
-                   .map(x => MAX_RGB *                    // scale components
-                             Math.max(0, Math.min(x, 1))) // Math.clamp(x, 0, 1)?
-               .join(' ')                                 // stringify line
-               .split(`^(?:.\{1,${MAX_LEN}\})\\s`))       // break up long lines
-           .join('\n') + '\n'                             // rejoin and terminate
-}
+                   .map(x => Math.round(MAX_RGB *          // scale components to int
+                             Math.max(0, Math.min(x, 1)))) // Math.clamp(x, 0, 1)?
+               .join(' ')                                  // stringify line
+               .split(/???MAX_LEN???/))                    // break up long lines
+           .join('\n') + '\n'                              // rejoin and terminate
+}"
 Object.defineProperties(exports.canvas.prototype, {
     'width':  { get: function() { return this.arr.find(Boolean).length } },
     'height': { get: function() { return this.arr.length } }
