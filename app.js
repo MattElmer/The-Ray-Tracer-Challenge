da06bed47b21c8f1a85efd5259bc29ea9c26f3b6
MattElmer <matthias.elmer@gmail.com>
2021-01-06 07:39:01 +0000

    tidying

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var a, p, v, c1, c2, r, s, xs, m, shape, comps, from, to, up, w
+var a, p, v, c1, c2, r, s, xs, m, shape, comps, from, to, up, w // global test var dumping ground
 
 if (!process.argv[2]) process.exit() // don't run if nothing passed in
 
-const { point, vector, color } = require('./tuple')
-const { sphere } = require('./sphere')
+const fs = require('fs')
+const { world } = require('./world')
 const { canvas } = require('./canvas')
 const { camera } = require('./camera')
-const { world } = require('./world')
+const { sphere } = require('./sphere')
+const { point, vector, color } = require('./tuple')
 const { point_light, material } = require('./lighting')
 const { translation, scaling, rotation_x, rotation_y, rotation_z, view_transformation } = require('./transformation')
-const fs = require('fs')
 
 let floor = new sphere
 floor.transform = scaling(10, 0.01, 10)
@@ -18,15 +18,11 @@ floor.material.color = color(1, 0.9, 0.9)
 floor.material.specular = 0
 
 let left_wall = new sphere
-left_wall.transform = translation(0, 0, 5).mul(
-                       rotation_y(-Math.PI/4).mul(rotation_x(Math.PI/2).mul(
-                       scaling(10, 0.01, 10))))
+left_wall.transform = translation(0, 0, 5).mul(rotation_y(-Math.PI/4).mul(rotation_x(Math.PI/2).mul(scaling(10, 0.01, 10))))
 left_wall.material = floor.material
 
 let right_wall = new sphere
-right_wall.transform = translation(0, 0, 5).mul(
-                        rotation_y(Math.PI/4).mul(rotation_x(Math.PI/2).mul(
-                        scaling(10, 0.01, 10))))
+right_wall.transform = translation(0, 0, 5).mul(rotation_y(Math.PI/4).mul(rotation_x(Math.PI/2).mul(scaling(10, 0.01, 10))))
 right_wall.material = floor.material
 
 let middle = new sphere
@@ -54,10 +50,8 @@ let scene = new world
 scene.objects = [floor, left_wall, right_wall, middle, left, right]
 scene.light = new point_light(point(-10, 10, -10), color(1, 1, 1))
 
-let eye = new camera(200, 100, Math.PI/3)
-eye.transform = view_transformation(point(0, 1.5, -5),
-                                   point(0, 1, 0),
-                                   vector(0, 1, 0))
+let eye = new camera(100, 50, Math.PI/3)
+eye.transform = view_transformation(point(0, 1.5, -5), point(0, 1, 0), vector(0, 1, 0))
 
 //# render the result to a canvas.
 let image = eye.render(scene)
