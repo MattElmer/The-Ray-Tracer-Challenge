957b256f5239d767acfe0d988332912e86f4a401
MattElmer <matthias.elmer@gmail.com>
2021-01-07 00:59:03 +0000

    style

--- intersection.js
+++ intersection.js
@@ -12,12 +12,12 @@ exports.intersections = (...xs) => xs.sort((a, b) => a - b)
 exports.hit = xs => xs[xs.findIndex(x => x > 0)]
 
 exports.prepare_computations = (i, r) => {
-    let comps        = { t:i.t, object:i.object }
-    comps.point      = r(comps.t)
-    comps.eyev       = neg(direction(r))
-    comps.normalv    = sphere.normal_at(comps.object, comps.point)
-    comps.inside     = dot(comps.normalv, comps.eyev) < 0
-    if (comps.inside) comps.normalv = neg(comps.normalv)
-    comps.over_point = add(comps.point, mul(comps.normalv, EPSILON))
+       let comps            = { t:i.t, object:i.object }
+           comps.point      = r(comps.t)
+           comps.eyev       = neg(direction(r))
+           comps.normalv    = sphere.normal_at(comps.object, comps.point)
+       if (comps.inside     = dot(comps.normalv, comps.eyev) < 0) {
+           comps.normalv    = neg(comps.normalv) }
+           comps.over_point = add(comps.point, mul(comps.normalv, EPSILON))
     return comps
 }
